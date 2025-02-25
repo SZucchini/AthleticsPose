0cc96a8bc777770fec9b9eac5723055bd0e8c542
SZucchini <suzuki.tomohiro@g.sp.m.is.nagoya-u.ac.jp>
2025-02-25 11:49:04 +0900

    add camera to pixel3d method

--- athleticspose/camera/camera.py
+++ athleticspose/camera/camera.py
@@ -148,7 +148,46 @@ class Camera:
 
         intrinsics = self.camera_params[cam_idx]["affine_intrinsics_matrix"]
         frames, joints, _ = kpts_cam.shape
+        kpts_cam_reshaped = kpts_cam.reshape(-1, 3)
         kpts_image = np.zeros((frames * joints, 2))
-        kpts_image[:, 0] = intrinsics[0][0] * (kpts_cam[:, 0] / kpts_cam[:, 2]) + intrinsics[0][2]
-        kpts_image[:, 1] = intrinsics[1][1] * (kpts_cam[:, 1] / kpts_cam[:, 2]) + intrinsics[1][2]
+        kpts_image[:, 0] = intrinsics[0][0] * (kpts_cam_reshaped[:, 0] / kpts_cam_reshaped[:, 2]) + intrinsics[0][2]
+        kpts_image[:, 1] = intrinsics[1][1] * (kpts_cam_reshaped[:, 1] / kpts_cam_reshaped[:, 2]) + intrinsics[1][2]
         return kpts_image.reshape(frames, joints, 2)
+
+    def camera_to_pixel3d(
+        self, kpts_cam: np.ndarray, cam_idx: int, root_idx: int = 0
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """Transform 3D keypoints from camera coordinates to pixel coordinates with depth.
+
+        Args:
+            kpts_cam (np.ndarray): Camera coordinates of keypoints with shape (T, J, 3).
+            cam_idx (int): The index of the camera to use.
+            root_idx (int): Index of the root joint.
+
+        Returns:
+            tuple[np.ndarray, np.ndarray]:
+                - kpts_pixel (np.ndarray): Pixel coordinates with scaled depth. Shape is (T, J, 3).
+                - scales (np.ndarray): Scale factors for each frame. Shape is (T,).
+
+        """
+        if cam_idx not in self.camera_params:
+            raise ValueError(f"Invalid camera index: {cam_idx}")
+
+        frames, _, _ = kpts_cam.shape
+        kpts_pixel = np.zeros_like(kpts_cam)
+        scales = np.zeros(frames)
+
+        intrinsics = self.camera_params[cam_idx]["affine_intrinsics_matrix"]
+        kpts_pixel[..., 0] = intrinsics[0][0] * (kpts_cam[..., 0] / kpts_cam[..., 2]) + intrinsics[0][2]
+        kpts_pixel[..., 1] = intrinsics[1][1] * (kpts_cam[..., 1] / kpts_cam[..., 2]) + intrinsics[1][2]
+
+        root_joints = kpts_cam[:, root_idx, :]
+        for i in range(frames):
+            ref_near = root_joints[i] - np.array([1000, 0, 0])
+            ref_far = root_joints[i] + np.array([1000, 0, 0])
+            x_near = intrinsics[0][0] * (ref_near[0] / ref_near[2]) + intrinsics[0][2]
+            x_far = intrinsics[0][0] * (ref_far[0] / ref_far[2]) + intrinsics[0][2]
+            scales[i] = (x_far - x_near) / 2000
+            kpts_pixel[i, :, 2] = (kpts_cam[i, :, 2] - root_joints[i, 2]) * scales[i]
+
+        return kpts_pixel, scales
